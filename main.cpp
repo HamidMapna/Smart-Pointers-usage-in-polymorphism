1aecbb8b84b5cb90957a505ab42e9f0327a437cf
Hamid <hamid.mah5400@gmail.com>
2024-01-21 13:57:59 +0330

    Remove explicit keyword due to its useless usage.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <memory>
 using namespace std;
-typedef int (*operation)(int, int);
-int add(int a, int b){
-    return a + b;
-}
-int subtract(int a, int b){
-    return a - b;
-}
+
+    typedef int (*operation)(int, int);
+
+    int add(int a, int b){
+        return a + b;
+    }
+    int subtract(int a, int b){
+        return a - b;
+    }
     class base_object{
         public:
             virtual int evaluate() = 0;
@@ -17,8 +19,7 @@ int subtract(int a, int b){
         private:
             int value;
         public:
-            primitive(int _value): value(_value){}
-            primitive(primitive&& primitive):value(std::move(primitive.value)){}
+            primitive(int _value): value(_value){}          
             int evaluate() override{
                return value; 
             }
@@ -29,7 +30,7 @@ int subtract(int a, int b){
             std::shared_ptr<base_object> left,right;
             operation op;
         public:
-         explicit composite() = delete;
+         composite() = delete;
          composite(std::shared_ptr<base_object> _left, std::shared_ptr<base_object> _right, operation _op):
                     left(std::move(_left)), right(std::move(_right)), op(_op){
                     std::cout<<"composite moving constructor was called"<<std::endl;
@@ -39,12 +40,12 @@ int subtract(int a, int b){
                 return op(left->evaluate(), right->evaluate());
          }
     };
-int main(){//calculate expression ((10+2)-5)
-    std::shared_ptr<composite> exp1 = std::make_shared<composite>
-                               (std::make_shared<primitive>(10), 
-                               std::make_shared<primitive>(2), &add);
-    std::shared_ptr<composite> exp2 = std::make_shared<composite>(std::move(exp1), std::make_shared<primitive>(5), &subtract);
-    int result = exp2->evaluate();
-    std::cout<<"final result of expression="<<result<<std::endl;
-    return 0;   
-}
+    int main(){//calculate expression ((10+2)-5)
+        std::shared_ptr<composite> exp1 = std::make_shared<composite>
+                                (std::make_shared<primitive>(10), 
+                                std::make_shared<primitive>(2), &add);
+        std::shared_ptr<composite> exp2 = std::make_shared<composite>(std::move(exp1), std::make_shared<primitive>(5), &subtract);
+        int result = exp2->evaluate();
+        std::cout<<"final result of expression="<<result<<std::endl;
+        return 0;   
+    }
