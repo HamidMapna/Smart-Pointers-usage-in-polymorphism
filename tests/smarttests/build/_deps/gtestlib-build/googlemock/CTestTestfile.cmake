fa14cd97c5f6caf08045fd0c2ed3e40e307474dd
Hamid <hamid.mah5400@gmail.com>
2024-01-23 12:27:07 +0330

    CMakelists file changed

--- tests/smarttests/build/_deps/gtestlib-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Source Codes/codePracticing/Composite_DP/Smart-Pointers-usage-in-polymorphism/tests/smarttests/build/_deps/gtestlib-src/googlemock
-# Build directory: D:/Source Codes/codePracticing/Composite_DP/Smart-Pointers-usage-in-polymorphism/tests/smarttests/build/_deps/gtestlib-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- tests/smarttests/build/_deps/gtestlib-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Source Codes/codePracticing/Composite_DP/Smart-Pointers-usage-in-polymorphism/tests/smarttests/build/_deps/gtestlib-src/googletest
-# Build directory: D:/Source Codes/codePracticing/Composite_DP/Smart-Pointers-usage-in-polymorphism/tests/smarttests/build/_deps/gtestlib-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
