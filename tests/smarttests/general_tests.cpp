0eae4f103b7ae7f682d6f54eb0c34510c859e035
Hamid <hamid.mahmoudi@sw-motion.cn>
2024-01-25 16:59:11 +0330

    failed in local, test on remote

--- tests/smarttests/general_tests.cpp
+++ tests/smarttests/general_tests.cpp
@@ -8,10 +8,10 @@ TEST(GeneralTests, simpleTest){
     std::shared_ptr<composite> exp1 = std::make_shared<composite>
                     (std::make_shared<primitive>(10), std::make_shared<primitive>(2), &add);
 
-   // std::shared_ptr<composite> exp2 = std::make_shared<composite>(std::move(exp1), std::make_shared<primitive>(5), &subtract);
+    std::shared_ptr<composite> exp2 = std::make_shared<composite>(std::move(exp1), std::make_shared<primitive>(5), &subtract);
     //ACT
 
-    double actual = exp1->evaluate();
+    double actual = exp2->evaluate();
     //ASSERT
     EXPECT_EQ(expected, actual);
 }
