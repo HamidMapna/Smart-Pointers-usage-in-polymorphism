ce143b24c0949e39e9d9b190d41b21ea258cdd91
Hamid <hamid.mahmoudi@sw-motion.cn>
2024-01-25 18:29:06 +0330

    changes commited.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "Composite.hpp"
+//#include "src/Composite.hpp"
     
     int main(){//calculate expression ((10+2)-5)
-        std::shared_ptr<composite> exp1 = std::make_shared<composite>
+       /* std::shared_ptr<composite> exp1 = std::make_shared<composite>
                                 (std::make_shared<primitive>(10), 
                                 std::make_shared<primitive>(2), &add);
         std::shared_ptr<composite> exp2 = std::make_shared<composite>(std::move(exp1), std::make_shared<primitive>(5), &subtract);
-        double result = exp2->evaluate();
-        std::cout<<"final result of expression="<<result<<std::endl;
+        double result = exp2->evaluate();*/
+        std::cout<<"final result of expression="<<std::endl;
         return 0;   
     }
